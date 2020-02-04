5abcee85d080ed8bbb7d7aa4cefbf499db73d618
mrmojica <mrmojica510@gmail.com>
2020-02-03 18:11:17 -0800

    add basic rank component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkeForm";
+import Rank from "./components/Rank/Rank";
 import "tachyons";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
