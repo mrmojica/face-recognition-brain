02bc211245616839c5d678079af05b498f3a460f
mrmojica <mrmojica510@gmail.com>
2020-02-03 19:46:13 -0800

    add background particles to app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkeForm";
 import Rank from "./components/Rank/Rank";
 import "tachyons";
+import Particles from "react-particles-js";
+
+// TODO: Improve particles
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
