831b3c1c92a3067e9587ba0e1526df95d5cb0b14
mrmojica <mrmojica510@gmail.com>
2020-02-20 16:35:19 -0800

    WIP - add clarifai api and wire up input/submit button

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkeForm";
 import Rank from "./components/Rank/Rank";
 import "tachyons";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 
-// TODO: Improve particles
+const CLARAFAI_API_KEY = process.env.REACT_APP_CLARIFAI_API_KEY;
+
+const app = new Clarifai.App({
+  apiKey: CLARAFAI_API_KEY
+});
+
 const particlesOptions = {
   particles: {
     number: {
@@ -27,17 +33,52 @@ const particlesOptions = {
   }
 };
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = React.useState("");
+
+  const onInputChange = event => {
+    console.log("value", event.target.value);
+    setInputValue(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    if (!CLARAFAI_API_KEY) {
+      throw new Error("You must set environment variable for clarafai apiKey");
+    }
+
+    console.log("submitted");
+    console.log(CLARAFAI_API_KEY);
+
+    app.models
+      .predict(
+        Clarifai.GENERAL_MODEL,
+        "https://samples.clarifai.com/metro-north.jpg",
+        { language: "en" }
+      )
+      .then(
+        function(response) {
+          // do something with response
+          console.log(response);
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+  };
+
   return (
     <div className="App">
       <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
       {/* <FaceRecognition /> */}
     </div>
   );
-}
+};
 
 export default App;
