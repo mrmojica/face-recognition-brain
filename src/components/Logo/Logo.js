d5d16aa42beeee87ef46fd00a0c401c224101ac3
mrmojica <mrmojica510@gmail.com>
2020-02-26 17:20:28 -0800

    remove unneeded code

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,20 +3,18 @@ import Tilt from "react-tilt";
 import brain from "./brain.png";
 import "./Logo.css";
 
-const Logo = () => {
-  return (
-    <div className="ma4 mt0">
-      <Tilt
-        className="Tilt br2 shadow-2"
-        options={{ max: 55 }}
-        style={{ height: 150, width: 150 }}
-      >
-        <div className="Tilt-inner pa3">
-          <img src={brain} alt="logo" style={{ paddingTop: "5px" }} />
-        </div>
-      </Tilt>
-    </div>
-  );
-};
+const Logo = () => (
+  <div className="ma4 mt1">
+    <Tilt
+      className="Tilt br2 shadow-2"
+      options={{ max: 55 }}
+      style={{ height: 150, width: 150 }}
+    >
+      <div className="Tilt-inner pa3">
+        <img src={brain} alt="logo" style={{ paddingTop: "5px" }} />
+      </div>
+    </Tilt>
+  </div>
+);
 
 export default Logo;
