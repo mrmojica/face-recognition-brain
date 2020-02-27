7e05630814bfaca33b0db1d11055807f24f71bc2
mrmojica <mrmojica510@gmail.com>
2020-02-26 17:40:43 -0800

    clean up FaceRecognition component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,21 +1,24 @@
 import React from "react";
-import "./FaceRecognition.css"
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ boxList, imageUrl }) => {
-  return (
-    <div className="center ma">
-      <div className="absolute mt2">
-        <img
-          id="inputImage"
-          src={imageUrl}
-          alt=""
-          width="500px"
-          height="auto"
+const FaceRecognition = ({ boxList, imageUrl }) => (
+  <div className="center ma">
+    <div className="absolute mt2">
+      <img id="inputImage" src={imageUrl} alt="" width="500px" height="auto" />
+      {boxList.map((box, index) => (
+        <div
+          key={index}
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
         />
-      {boxList.map((box, index) => <div key={index} className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}} /> )}
-      </div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default FaceRecognition;
